34550677634e421aa41997562279b54d0b62347f
Sajjad Ahmad <external.ahmad_sajjad@allianzdirect.de>
2024-01-26 09:59:06 +0100

    refactor: change name

--- src/main.js
+++ src/main.js
@@ -4,24 +4,24 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
-		// In the following fiels you can either give a single string, 
+		name: 'Max Power',
+		// In the following fiels you can either give a single string,
 		// or an array of bullet points
-		
+
 		// What do you associate with the term 'CI/CD'?
 		associations: [
 			'Ehm, what?'
 		],
 		// Which CI/CD tools do you use in your project?
 		tools: 'Jenkins, maybe?',
-		// What do you want to learn in this workshop? 
+		// What do you want to learn in this workshop?
 		expectations: [
 			'what you are talking about'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
+			'play chess',
+			'do skydiving',
 			'do my taxes'
 		]
 	}
